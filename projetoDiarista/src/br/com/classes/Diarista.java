0ee00fa044b0bb04d7146699c290aeecf6479e8a
Carlos.E <134007024+Car-Garcia@users.noreply.github.com>
2023-07-25 11:44:52 -0300

    Projeto diarista

---
+++ projetoDiarista/src/br/com/classes/Diarista.java
@@ -0,0 +1,10 @@
+package br.com.classes;
+
+public class Diarista extends Pessoa{
+	
+	public String chavePix;
+
+	public void atender(String nomeCliente) {
+		System.out.println("Realizando atendimento para o cliente: " + nomeCliente);
+	}
+}
